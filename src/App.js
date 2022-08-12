1a78ea8a7314aeee7064ba17afc53bee0a71e6e8
doandnguyen <doan.nguyen@innomizetech.com>
2022-08-12 15:13:24 +0700

    Testing feature001

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Wellcome to gitFlow gitAction
+	  Testing feature001
         </p>
         <a
           className="App-link"
